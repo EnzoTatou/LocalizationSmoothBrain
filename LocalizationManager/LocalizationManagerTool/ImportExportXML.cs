8bca1ad7a559c5221c0c69930739b1972625b98c
Olivier Maurin <154346476+Toraenor@users.noreply.github.com>
2024-10-29 16:57:15 +0100

    c fini hihi

--- LocalizationManager/LocalizationManagerTool/ImportExportXML.cs
+++ LocalizationManager/LocalizationManagerTool/ImportExportXML.cs
@@ -1,13 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace LocalizationManagerTool
 {
+    public struct ColumnStruct
+    {
+        public int ID { get; set; }
+        public string Languages { get; set; }
+    }
+
     public partial class MainWindow
     {
+        public List<ColumnStruct> ImportFromXML(string filePath)
+        {
+            var serializer = new XmlSerializer(typeof(List<ColumnStruct>));
+            using (var reader = new StreamReader(filePath))
+            {
+                if(serializer != null)
+                {
+                    object? deserializedContent = serializer.Deserialize(reader);
 
+                    if (deserializedContent != null)
+                    {
+                        return (List<ColumnStruct>)deserializedContent;
+                    }
+                }
+            }
+            return new List<ColumnStruct>();
+        }
+
+        public void ExportToXML(DataGrid dataGrid, string filePath)
+        {
+            var items = dataGrid.Items.Cast<ColumnStruct>().ToList();
+            var serializer = new XmlSerializer(typeof(List<ColumnStruct>));
+            using (var writer = new StreamWriter(filePath))
+            {
+                serializer.Serialize(writer, items);
+            }
+        }
     }
 }
