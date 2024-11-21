b0146d14a566cb47669ac7bfb73fbe8cda3d899a
Maurin Olivier <154346476+Toraenor@users.noreply.github.com>
2024-11-21 19:50:09 +0100

    Update ImportExportXML.cs

--- LocalizationManager/LocalizationManagerTool/ImportExportXML.cs
+++ LocalizationManager/LocalizationManagerTool/ImportExportXML.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Xml;
 using System.Xml.Linq;
 using System.Xml.Serialization;
 
@@ -55,11 +56,44 @@ namespace LocalizationManagerTool
 
         public void ExportToXML(DataGrid dataGrid, string filePath)
         {
-            var items = dataGrid.ItemsSource.Cast<Row>().ToList();
-            var serializer = new XmlSerializer(typeof(List<Row>));
-            using (var writer = new StreamWriter(filePath))
+            Dictionary<string, Dictionary<string, string>> localizerDictionaries = new();
+
+            foreach(string language in GetLanguages())
             {
-                serializer.Serialize(writer, items);
+                localizerDictionaries.Add(language,new Dictionary<string, string>());
+            }
+
+            foreach (var row in rows)
+            {
+                string id = row.Languages["id"];
+                foreach (var key in row.Languages.Keys)
+                {
+                    if (key == "id") continue;
+                    localizerDictionaries[key][id] = row.Languages[key];
+                }
+            }
+
+            // Create an XML file manually
+            using (var writer = XmlWriter.Create(filePath, new XmlWriterSettings { Indent = true }))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Languages"); // Root element
+
+                foreach (string language in localizerDictionaries.Keys)
+                {
+                    writer.WriteStartElement("Language", language);
+
+                    foreach(var idValuePair in localizerDictionaries[language])
+                    {
+                        writer.WriteElementString("Id", idValuePair.Key);
+                        writer.WriteElementString("Value", idValuePair.Value);
+                    }
+
+                    writer.WriteEndElement(); // </Language>
+                }
+
+                writer.WriteEndElement(); // </Languages>
+                writer.WriteEndDocument();
             }
         }
     }
